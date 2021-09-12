55ee460b9405b764ef3f768aa9fc5f431daaf561
Derimar Gray <derimargray@gmail.com>
2021-09-12 08:58:24 -0500

    add employee

--- index.js
+++ index.js
@@ -68,10 +68,9 @@ function init() {
         case "view_all_employees":
           viewAllEmployees();
           break;
-
-        // case "add_an_employee":
-        //   addAnEmployee();
-        //   break;
+        case "add_an_employee":
+          addAnEmployee();
+          break;
         // case "update_an_employee_role":
         //   updateAnEmployeeRole();
         //   break;
@@ -101,7 +100,8 @@ function addADepartment() {
         name: "name",
         message: "Provide department name!",
       },
-    ]).then((res) => {
+    ])
+    .then((res) => {
       let name = res;
       db.addDepartment(name)
         .then(() =>
@@ -136,36 +136,38 @@ function addARole() {
       value: id,
     }));
 
-    inquirer.prompt([
-      {
-        name: "title",
-        message: "Provide name of the role!",
-      },
-      {
-        name: "salary",
-        message: "Provide salary rate!",
-      },
-      {
-        type: "list",
-        name: "department_id",
-        message: "Provide department of role!",
-        choices: departmentOptions,
-      },
-    ]).then(role => {
-      db.addRole(role)
-        .then(() =>
-          console.log(`
+    inquirer
+      .prompt([
+        {
+          name: "title",
+          message: "Provide name of the role!",
+        },
+        {
+          name: "salary",
+          message: "Provide salary rate!",
+        },
+        {
+          type: "list",
+          name: "department_id",
+          message: "Provide department of role!",
+          choices: departmentOptions,
+        },
+      ])
+      .then((role) => {
+        db.addRole(role)
+          .then(() =>
+            console.log(`
 ==========================================
 ${role.title} role added to database!
 ==========================================`)
-        )
-        .then(() => console.log("\n"))
-        .then(() => init());
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  })
+          )
+          .then(() => console.log("\n"))
+          .then(() => init());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 }
 
 function viewAllEmployees() {
@@ -176,6 +178,85 @@ function viewAllEmployees() {
       console.table(employees);
     })
     .then(() => init());
+}
+
+function addAnEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "first_name",
+        message: "Provide employee first name!",
+      },
+      {
+        name: "last_name",
+        message: "Provide employee last name!",
+      },
+    ])
+    .then((res) => {
+      let firstName = res.first_name;
+      let lastName = res.last_name;
+
+      db.allRoles().then(([rows]) => {
+        let roles = rows;
+        const roleOptions = roles.map(({ id, title }) => ({
+          name: title,
+          value: id,
+        }));
+
+        inquirer
+          .prompt({
+            type: "list",
+            name: "roleId",
+            message: "Select Employee Role!",
+            choices: roleOptions,
+          })
+          .then((res) => {
+            let roleId = res.roleId;
+
+            db.allEmployees()
+              .then(([rows]) => {
+                let employees = rows;
+                const managerOptions = employees.map(
+                  ({ id, first_name, last_name }) => ({
+                    name: `${first_name} ${last_name}`,
+                    value: id,
+                  })
+                );
+
+                managerOptions.unshift({ name: "None", value: null });
+
+                inquirer
+                  .prompt({
+                    type: "list",
+                    name: "managerId",
+                    message: "Provide employee's manager",
+                    choices: managerOptions,
+                  })
+                  .then((res) => {
+                    let employee = {
+                      manager_id: res.managerId,
+                      role_id: roleId,
+                      first_name: firstName,
+                      last_name: lastName,
+                    };
+
+                    db.addEmployee(employee);
+                  })
+                  .then(() =>
+                    console.log(`
+=====================================================
+${firstName} ${lastName}, employee added to database!
+=====================================================`)
+                  )
+                  .then(() => console.log("\n"))
+                  .then(() => init());
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+      });
+    });
 }
 
 function exitTracker() {
